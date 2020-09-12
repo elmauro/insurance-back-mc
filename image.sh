a6bb262cbbf9546e1b2e8e1cc955fe152f0ce63c
Mauricio Cadavid <mauricio.cadavid@puntoscolombia.com>
2020-09-12 15:45:55 -0500

    Files for Deploying into AWS

--- image.sh
+++ image.sh
@@ -10,7 +10,7 @@ if [ ! -z "$CONTAINERS" ]; then docker stop $CONTAINERS; docker rm -f $CONTAINER
 if [ ! -z "$IMAGEID" ]; then docker rmi -f $IMAGEID; fi;
 docker build -t insurance-back-mc .;
 
-docker tag insurance-back-mc:latest 323700164624.dkr.ecr.us-east-1.amazonaws.com/insurance-back-mc:latest;
+docker tag insurance-back-mc:latest 728401402215.dkr.ecr.us-east-1.amazonaws.com/insurance-back-mc:latest;
 
 # docker run -e ASPNETCORE_ENVIRONMENT=Production --name insurance-back -p 5000:80 insurance-back-mc;
 # docker run -e ASPNETCORE_ENVIRONMENT=Development --name insurance-back -p 5000:80 insurance-back-mc;
